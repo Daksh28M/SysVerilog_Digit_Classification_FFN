56c2579f3b75d6fe870ce96a3f2161740f4006e4
Daksh28M <daksh6382@gmail.com>
2026-06-08 12:56:09 +0530

    added neuron module

---
+++ Digit_Classification_FFN.srcs/sources_1/new/neuron.sv
@@ -0,0 +1,63 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 08.06.2026 12:24:26
+// Design Name: 
+// Module Name: neuron
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module neuron #(parameter IN_SIZE = 196, WIDTH = 8)(
+    input clk,
+    input en,
+    input rst,
+    input signed [2*WIDTH-1:0] in_data [1:IN_SIZE-1],
+    input signed [WIDTH-1:0] weight [1:IN_SIZE-1],
+    input signed [WIDTH-1:0] bias,
+    output signed [4*WIDTH-1:0] neuron_out,
+    output neuron_done
+    );
+
+    integer addr = 0;
+    reg done = 0;
+
+    wire signed [4*WIDTH-1:0] product_w;
+    reg signed [4*WIDHT-1:0] out;
+
+    assign product_w = in_data[addr] * weight[addr];
+
+    always @(posedge clk) begin
+        if(reset) begin
+            addr <= 0;
+            done <= 1'b0;
+            out <= 0;
+        end
+        else if(en) begin
+            if(addr < IN_SIZE) begin
+                out <= out + product_w;
+                addr <= addr + 1;
+                done <= 1'b0;
+            end
+            else if(addr == IN_SIZE) begin
+                done <= 1'b1;
+            end
+        end
+    end
+
+    assign neuron_out = out + bias;
+    assign neuron_done = done;
+    
+endmodule
