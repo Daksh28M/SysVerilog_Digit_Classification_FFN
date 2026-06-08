06da50a6c0871c2fa7253fa02d8d5a8756b616fc
Daksh28M <daksh6382@gmail.com>
2026-06-08 12:22:51 +0530

    created img pooling modules

--- Digit_Classification_FFN.srcs/sources_1/new/avg_pooling.sv
+++ Digit_Classification_FFN.srcs/sources_1/new/avg_pooling.sv
@@ -21,6 +21,30 @@
 
 
 module avg_pooling(
-
+    input clk,
+    input pool_en,
+    input [7:0] in1,
+    input [7:0] in2,
+    input [7:0] in3,
+    input [7:0] in4,
+    output [7:0] out,
+    output pool_done
     );
+
+    reg [15:0] pool_out;
+    reg done;
+
+    always @(posedge clk) begin
+        if(pool_en) begin
+            pool_out <= ( in1 + in2 + in3 + in4 ) >> 2;
+            done <= 1'b1;
+        end
+        else begin
+            done <= 1'b0;
+        end
+    end
+
+    assign out = pool_out[7:0];
+    assign pool_done = done;
+    
 endmodule
